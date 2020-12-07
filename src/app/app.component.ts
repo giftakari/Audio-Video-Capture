5b3236999faa4e5de4b9b4ec322c8e8380018b71
giftakari@yahoo.com <giftakari@yahoo.com>
2020-12-07 11:23:14 +1000

    add routing to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RecordRTCPromisesHandler, RecordRTC} from 'recordrtc'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RecordRTC-Angular';
+
+
+
+  startRecord() {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+  }).then((stream)=> {
+      let recorder = RecordRTC(stream, {
+          type: 'video'
+      });
+      recorder.startRecording();
+ 
+    }
+    )
+  
+  }
+
 }
